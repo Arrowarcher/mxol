f5f758a545379998c82075f71701d600bd3d5d4c
Arrow <185542076@qq.com>
2019-01-03 17:16:26 +0800

    app_models ready!

---
+++ operation/models.py
@@ -0,0 +1,60 @@
+from django.db import models
+
+# Create your models here.
+from courses.models import Course
+from users.models import UserProfile
+
+
+class UserAsk(models.Model):
+    name = models.CharField(max_length=20, verbose_name="姓名")
+    mobile = models.CharField(max_length=11, verbose_name="手机")
+    course_name = models.CharField(max_length=50, verbose_name="课程名")
+    add_time = models.DateTimeField(auto_now_add=True, verbose_name="添加时间")
+
+    class Meta:
+        verbose_name = "用户咨询"
+        verbose_name_plural = verbose_name
+
+
+class CourseComments(models.Model):
+    "课程评论"
+    user = models.ForeignKey(UserProfile, verbose_name="用户",on_delete=models.SET_NULL)
+    course = models.ForeignKey(Course, verbose_name="课程",on_delete=models.SET_NULL)
+    comments = models.CharField(max_length=200, verbose_name="评论")
+    add_time = models.DateTimeField(auto_now_add=True, verbose_name="添加时间")
+
+    class Meta:
+        verbose_name = "课程评论"
+        verbose_name_plural = verbose_name
+
+
+class UserFavorite(models.Model):
+    user = models.ForeignKey(UserProfile, verbose_name="用户",on_delete=models.SET_NULL)
+    fav_id = models.IntegerField(default=0, verbose_name="数据id")
+    fav_type = models.IntegerField(choices=((1,"课程"),(2,"课程机构"),(3,"讲师")), default=1, verbose_name="收藏类型")
+    add_time = models.DateTimeField(auto_now_add=True, verbose_name="添加时间")
+
+    class Meta:
+        verbose_name = "用户收藏"
+        verbose_name_plural = verbose_name
+
+
+class UserMessage(models.Model):
+    user = models.IntegerField(default=0, verbose_name="接收用户")
+    message = models.CharField(max_length=500, verbose_name="消息内容")
+    has_read = models.BooleanField(default=False, verbose_name="是否已读")
+    add_time = models.DateTimeField(auto_now_add=True, verbose_name="添加时间")
+
+    class Meta:
+        verbose_name = "用户消息"
+        verbose_name_plural = verbose_name
+
+
+class UserCourse(models.Model):
+    user = models.ForeignKey(UserProfile, verbose_name="用户",on_delete=models.SET_NULL)
+    course = models.ForeignKey(Course, verbose_name="课程",on_delete=models.SET_NULL)
+    add_time = models.DateTimeField(auto_now_add=True, verbose_name="添加时间")
+
+    class Meta:
+        verbose_name = "用户课程"
+        verbose_name_plural = verbose_name
