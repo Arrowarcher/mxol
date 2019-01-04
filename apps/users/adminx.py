2589cb3b3f26ad873a570b10903e4824384ce2c7
Arrow <185542076@qq.com>
2019-01-04 17:06:03 +0800

    侧边栏配置

--- apps/users/adminx.py
+++ apps/users/adminx.py
@@ -1,11 +1,29 @@
 import xadmin
 from users.models import EmailVerifyRecord, Banner
 
+
+from xadmin import views
+
+# xadmin的主题功能配置
+@xadmin.sites.register(views.BaseAdminView)
+class BaseSetting(object):
+    enable_themes = True
+    use_bootswatch = True
+
+
+#xadmin的全局样式配置
+@xadmin.sites.register(views.CommAdminView)
+class GlobalSettings(object):
+    site_title = "xadmin后台管理系统"
+    site_footer = "Arrowarcher"
+    menu_style = "accordion"
+
+
 @xadmin.sites.register(EmailVerifyRecord)
 class EmailVerifyRecordAdmin(object):
-    list_display = ['code', 'email','send_type', 'send_time']
-    search_fields = ['code', 'email','send_type']
-    list_filter = ['code', 'email','send_type', 'send_time']
+    list_display = ['code', 'email', 'send_type', 'send_time']
+    search_fields = ['code', 'email', 'send_type']
+    list_filter = ['code', 'email', 'send_type', 'send_time']
 
 
 @xadmin.sites.register(Banner)
